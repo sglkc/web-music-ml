eb50ebad395b820bb0dd448f57b09b9ae80e7376
Seya <31957516+sglkc@users.noreply.github.com>
2023-06-11 13:46:19 +0700

    feat: models

--- netlify/functions/db/models.js
+++ netlify/functions/db/models.js
@@ -10,193 +10,178 @@ const sequelize = new Sequelize({
   logging: process.env.NODE_ENV === 'development' && console.log
 });
 
-const Pengguna = sequelize.define('pengguna', {
-  id_pengguna: {
+const Artist = sequelize.define('artists', {
+  artist_id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     autoIncrement: true,
     primaryKey: true,
   },
-  username: {
-    type: DataTypes.STRING(20),
+  name: {
+    type: DataTypes.STRING(64),
     allowNull: false,
-    unique: {
-      msg: 'username sudah dipakai'
-    }
   },
-  password: {
-    type: DataTypes.STRING(60),
+  nationality: {
+    type: DataTypes.STRING(16),
     allowNull: false
   },
-  dibuat: {
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.fn('now'),
+    allowNull: false,
+  },
+  updated_at: {
     type: DataTypes.DATE,
     defaultValue: Sequelize.fn('now'),
     allowNull: false,
   },
 }, {
-  defaultScope: {
-    attributes: {
-      exclude: ['password']
-    }
-  },
-  timestamps: false,
-  tableName: 'pengguna',
-  createdAt: 'dibuat',
+  tableName: 'artists',
 });
 
-const Pos = sequelize.define('pos', {
-  id_pos: {
-    type: DataTypes.INTEGER,
+const Language = sequelize.define('languages', {
+  language_code: {
+    type: DataTypes.STRING(2),
     allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
+    primaryKey: true
   },
-  id_pengguna: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Pengguna,
-      key: 'id_pengguna',
-    }
-  },
-  isi: {
-    type: DataTypes.STRING(250),
-    allowNull: false,
-  },
-  dibuat: {
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.fn('now'),
-    allowNull: false,
-  },
-}, {
-  defaultScope: {
-    attributes: {
-      include: [
-        [sequelize.col('pengguna.username'), 'username'],
-        [sequelize.literal('(SELECT COUNT(*) FROM suka WHERE suka.id_pos = pos.id_pos)'), 'sukaCount'],
-      ]
-    },
-    include: [
-      {
-        association: 'pengguna',
-        attributes: []
-      },
-      'komentar'
-    ],
-  },
-  scopes: {
-    compact: {
-      attributes: {
-        include: [
-          [sequelize.col('pengguna.username'), 'username'],
-          [sequelize.literal('(SELECT COUNT(*) FROM suka WHERE suka.id_pos = pos.id_pos)'), 'sukaCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM komentar WHERE komentar.id_pos = pos.id_pos)'), 'komentarCount'],
-        ],
-        exclude: ['id_pengguna']
-      },
-      include: [
-        {
-          association: 'pengguna',
-          attributes: []
-        },
-      ],
-    }
-  },
-  timestamps: false,
-  tableName: 'pos',
-  createdAt: 'dibuat'
-});
-
-const Suka = sequelize.define('suka', {
-  id_pengguna: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    references: {
-      model: Pengguna,
-      key: 'id_pengguna',
-    }
-  },
-  id_pos: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    references: {
-      model: Pos,
-      key: 'id_pos'
-    }
-  },
-}, {
-  defaultScope: {
-    attributes: {
-      exclude: ['id_pos', 'id_pengguna']
-    },
-    include: {
-      association: 'pengguna',
-      attributes: ['username']
-    }
-  },
-  timestamps: false,
-  tableName: 'suka',
-});
-
-const Komentar = sequelize.define('komentar', {
-  id_komentar: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  id_pengguna: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Pengguna,
-      key: 'id_pengguna',
-    }
-  },
-  id_pos: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Pos,
-      key: 'id_pos'
-    }
-  },
-  isi: {
-    type: DataTypes.STRING(250),
+  country: {
+    type: DataTypes.STRING(32),
     allowNull: false
+  }
+}, {
+  tableName: 'languages',
+  timestamps: false
+});
+
+const Metadata = sequelize.define('metadata', {
+  metadata_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  dibuat: {
+  length: {
+    type: DataTypes.TIME,
+    allowNull: false,
+  },
+  tempo: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  skey: { type: DataTypes.STRING(16), allowNull: false },
+  loudness: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  popularity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  energy: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  speechiness: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  instrumentalness: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  danceability: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  positiveness: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  liveness: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+}, {
+  tableName: 'metadata',
+  timestamps: false
+});
+
+const Song = sequelize.define('songs', {
+  song_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  artist_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Artist,
+      key: 'artist_id',
+    }
+  },
+  title: { type: DataTypes.STRING(64), allowNull: false },
+  spotify_url: { type: DataTypes.STRING(128) },
+  image_url: { type: DataTypes.STRING(128) },
+  language_code: {
+    type: DataTypes.STRING(2),
+    allowNull: false,
+    references: {
+      model: Language,
+      key: 'language_code'
+    }
+  },
+  metadata_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Metadata,
+      key: 'metadata_id'
+    }
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.fn('now'),
+    allowNull: false,
+  },
+  updated_at: {
     type: DataTypes.DATE,
     defaultValue: Sequelize.fn('now'),
     allowNull: false,
   },
 }, {
   defaultScope: {
-    attributes: {
-      exclude: ['id_pengguna', 'id_pos']
-    },
-    include: {
-      association: 'pengguna',
-      attributes: ['username']
-    },
+    include: [{ all: true }]
   },
-  timestamps: false,
-  tableName: 'komentar',
-  createdAt: 'dibuat'
+  tableName: 'songs'
 });
 
-Pengguna.hasMany(Pos, { foreignKey: 'id_pengguna' });
-Pos.belongsTo(Pengguna, { foreignKey: 'id_pengguna' });
-Pos.hasMany(Suka, { as: 'suka', foreignKey: 'id_pos' });
-Pos.hasMany(Komentar, { as: 'komentar', foreignKey: 'id_pos' });
-Suka.belongsTo(Pengguna, { foreignKey: 'id_pengguna' });
-Komentar.belongsTo(Pengguna, { foreignKey: 'id_pengguna' });
+const Genre = sequelize.define('genres', {
+  genre_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  subgenre_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Genre,
+      key: 'genre_id',
+    }
+  },
+  name: {
+    type: DataTypes.STRING(32),
+    allowNull: false
+  }
+}, {
+  tableName: 'genres',
+  timestamps: false
+});
+
+const SongGenres = sequelize.define('songgenres', {
+  song_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Song,
+      key: 'song_id',
+    }
+  },
+  genre_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Genre,
+      key: 'genre_id',
+    }
+  },
+}, {
+  tableName: 'songgenres',
+  timestamps: false
+});
+
+Song.hasOne(Artist, { foreignKey: 'artist_id' });
+Song.hasMany(Genre, { foreignKey: 'genre_id' });
+Song.hasOne(Language, { foreignKey: 'language_code' });
+Song.hasOne(Metadata, { foreignKey: 'artist_id' });
 
 module.exports = {
-  Pengguna,
-  Pos,
-  Suka,
-  Komentar
+  Artist,
+  Language,
+  Metadata,
+  Song,
+  Genre
 };
